f6d51b54a743dfb67aa9d73735db5a5a2a74a81a
MelonShooter <14815068+MelonShooter@users.noreply.github.com>
2021-06-21 15:09:01 -0700

    Add AES encode function

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,6 +3,7 @@ use std::fmt::Display;
 use aes::cipher::generic_array::GenericArray;
 use aes::Aes256;
 use aes::BlockDecrypt;
+use aes::BlockEncrypt;
 use aes::NewBlockCipher;
 
 #[allow(dead_code)]
@@ -44,4 +45,39 @@ pub fn decode_aes_bytes(encoded_input: Vec<u8>) -> String {
     }
 
     decoded_string
+}
+
+#[allow(dead_code)]
+pub fn encode_aes(str: String) -> String {
+    let mut string;
+    let mut str_bytes = str.as_bytes();
+    let pad_count = 16 - str.len() % 16;
+
+    if pad_count != 0 {
+        string = String::with_capacity(pad_count + str_bytes.len());
+
+        for _ in 0..pad_count {
+            string.push('0');
+        }
+
+        string.push_str(str.as_str());
+
+        str_bytes = string.as_bytes();
+    }
+
+    let key = hex::decode(crate::secret::AES_KEY).expect("Bad key.");
+    let cipher = Aes256::new_from_slice(key.as_slice()).expect("Bad key.");
+    let mut encoded_bytes = String::with_capacity(str_bytes.len() * 2 + 1);
+
+    encoded_bytes.push('f');
+
+    for start in (0..str_bytes.len()).step_by(16) {
+        let mut block = *GenericArray::from_slice(&str_bytes[start..start + 16]);
+
+        cipher.encrypt_block(&mut block);
+
+        encoded_bytes.push_str(hex::encode(block).as_str());
+    }
+
+    encoded_bytes
 }
